cfa07893bb2f1e2e1af988e7f31343fe8d079771
shivangi8293 <shivangi.johri@posten.no>
2021-01-21 17:43:22 +0530

    Working demo of building virtual network

--- workingDemoBuildingInfrastructure/main.tf
+++ workingDemoBuildingInfrastructure/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "dummy" {
 
 
 /*
-resource "azurerm_virtual_network" "test1" {
-  name                = "test1"
+resource "azurerm_virtual_network" "test" {
+  name                = "test"
   location            = azurerm_resource_group.dummy.location
   resource_group_name = azurerm_resource_group.dummy.name
   address_space       = ["10.0.0.0/16"]
