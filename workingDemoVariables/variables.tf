53844db8f094c0434b37390690f06ed8da53421d
shivangi8293 <shivangi.johri@posten.no>
2021-01-27 03:38:22 +0530

    Working demo of variables

--- workingDemoVariables/variables.tf
+++ workingDemoVariables/variables.tf
@@ -4,3 +4,21 @@ variable "loc" {
 variable "rg" {
   default = "dummy1"
 }
+variable "name" {
+  default = "dummy rg"
+}
+variable "space" {
+  default = 20
+}
+variable "active" {
+  default = false
+}
+variable "locations"{
+  default = ["Central India" , "West India"]
+}
+variable "tags" {
+  type = map
+  default = {
+    environment = "test"
+  }
+}
